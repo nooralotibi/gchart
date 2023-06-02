1f6747fa370133b9f2595f08e9e615c822db5997
nooreealotibi <nooreealotibi@gmail.com>
2023-06-02 13:09:55 +0300

    Initial Commit

---
+++ gchart/gchartApp.swift
@@ -0,0 +1,17 @@
+//
+//  gchartApp.swift
+//  gchart
+//
+//  Created by noor alotibi on 13/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct gchartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
